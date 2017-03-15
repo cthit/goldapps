e4644be884af599ddf3a4f6c25a84d25d16e00c9
Gurgy <Gurgy@emycia.com>
2017-03-15 22:51:48 +0100

    Change config format

--- collector.go
+++ collector.go
@@ -11,22 +11,22 @@ import (
 
 func getLDAPGroups() {
 
-	l, err := ldap.DialTLS("tcp", viper.GetString("URL"), &tls.Config{ServerName: viper.GetString("ServerName")})
+	l, err := ldap.DialTLS("tcp", viper.GetString("ldap.url"), &tls.Config{ServerName: viper.GetString("ldap.servername")})
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer l.Close()
 
-	err = l.Bind(viper.GetString("User"), viper.GetString("Password"))
+	err = l.Bind(viper.GetString("ldap.user"), viper.GetString("ldap.assword"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	searchRequest := ldap.NewSearchRequest(
-		viper.GetString("BaseDN"), // The base dn to search
+		viper.GetString("ldap.basedn"), // The base dn to search
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		viper.GetString("Filter"),          // The filter to apply
-		viper.GetStringSlice("Attributes"), // A list attributes to retrieve
+		viper.GetString("ldap.filter"),          // The filter to apply
+		viper.GetStringSlice("ldap.Attributes"), // A list attributes to retrieve
 		nil,
 	)
 
