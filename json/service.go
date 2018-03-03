a65867cad0ab3131f440e0a2c4d539f29be6f9af
Gurgy <gurgy@emycia.com>
2018-03-03 23:46:11 +0100

    Add josn service

---
+++ json/service.go
@@ -0,0 +1,83 @@
+package json
+
+import (
+	"github.com/cthit/goldapps"
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
+)
+
+type jsonService struct {
+	path string
+}
+
+func NewJsonService(path string) (jsonService, error) {
+	return jsonService{
+		path: path,
+	}, nil
+}
+
+func (s jsonService) save(groups []goldapps.Group) error {
+	data, _ := json.Marshal(groups)
+
+	err := ioutil.WriteFile(s.path, data, 0600)
+	return err
+}
+
+func (s jsonService) DeleteGroup(group goldapps.Group) error {
+	groups, err := s.GetGroups()
+	if err != nil {
+		return err
+	}
+
+	for i,g := range groups {
+		if g.Email == group.Email{
+			err = s.save(append(groups[:i], groups[i+1:]...))
+			return err
+		}
+	}
+	return fmt.Errorf("group not found %v", group)
+}
+
+func (s jsonService) UpdateGroup(groupUpdate goldapps.GroupUpdate) error {
+	groups, err := s.GetGroups()
+	if err != nil {
+		return err
+	}
+
+	for i,g := range groups {
+		if g.Email == groupUpdate.Before.Email{
+			err = s.save(append(append(groups[:i], groupUpdate.After), groups[i+1:]...))
+			return err
+		}
+	}
+	return fmt.Errorf("group not found %v", groupUpdate.Before)
+}
+
+func (s jsonService) AddGroup(group goldapps.Group) error {
+	groups, err := s.GetGroups()
+	if err != nil {
+		return err
+	}
+
+	groups = append(groups, group)
+
+	err = s.save(groups)
+	return err
+}
+
+func (s jsonService) GetGroups() ([]goldapps.Group, error) {
+
+	bytes, err := ioutil.ReadFile(s.path)
+	if err != nil {
+		return nil, err
+	}
+
+	var data []goldapps.Group
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
