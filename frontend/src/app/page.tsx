fad14d58290bda843b3e2a620d53c72c0327589a
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-05-27 02:34:46 +0200

    fetch actual suggestions again

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,84 +1,24 @@
 import { Suggestion } from "../lib/goldapps/types";
 import { Suggestions } from "../components/Suggestions";
+import { cookies } from "next/headers";
+import { createGoldappsServerClient } from "../lib/goldapps/client";
+import { checkLogin } from "../lib/goldapps/auth";
+import { redirect } from "next/navigation";
+import { getSuggestions } from "../lib/goldapps/get-suggestions";
 
-async function getSuggestions() {
-  const mockSuggestions: Suggestion[] = [
-    {
-      type: "AddUser",
-      user: {
-        cid: "cid",
-        first_name: "first_name",
-        second_name: "second_name",
-        nick: "nick",
-        mail: "mail@chalmers.it",
-      },
-    },
-    {
-      type: "DeleteUser",
-      user: {
-        cid: "cid",
-        first_name: "first_name",
-        second_name: "second_name",
-        nick: "nick",
-        mail: "mail@chalmers.it",
-      },
-    },
-    {
-      type: "ChangeUser",
-      before: {
-        cid: "cid",
-        first_name: "first_name",
-        second_name: "second_name",
-        nick: "nick",
-        mail: "mail@chalmers.it",
-      },
-      after: {
-        cid: "newcid",
-        first_name: "newfirst_name",
-        second_name: "newsecond_name",
-        nick: "newnick",
-        mail: "newmail@chalmers.it",
-      },
-    },
-    {
-      type: "AddGroup",
-      group: {
-        email: "email@chalmers.it",
-        expendable: false,
-        type: "SOCIETY",
-        members: ["member1@chalmers.it"],
-      },
-    },
-    {
-      type: "DeleteGroup",
-      group: {
-        email: "email@chalmers.it",
-        expendable: false,
-        type: "SOCIETY",
-        members: ["member1@chalmers.it"],
-      },
-    },
-    {
-      type: "ChangeGroup",
-      before: {
-        email: "email@chalmers.it",
-        expendable: false,
-        type: "SOCIETY",
-        members: ["member1@chalmers.it", "member2@chalmers.it"],
-      },
-      after: {
-        email: "newemail@chalmers.it",
-        expendable: false,
-        type: "COMMITTEE",
-        members: ["member2@chalmers.it", "member3@chalmers.it"],
-      },
-    },
-  ];
+async function fetchSuggestions() {
+  const cookieJar = cookies();
+  const goldappsSessionCookie = cookieJar.get("goldapps-session")?.value || "";
+  const goldappsClient = createGoldappsServerClient(goldappsSessionCookie);
+  const redirectUri = await checkLogin(goldappsClient);
+  if (redirectUri) {
+    return redirect(redirectUri);
+  }
 
-  return mockSuggestions;
+  return getSuggestions(goldappsClient);
 }
 
 export default async function IndexPage() {
-  const suggestions = await getSuggestions();
+  const suggestions = await fetchSuggestions();
   return <Suggestions suggestions={suggestions} />;
 }
