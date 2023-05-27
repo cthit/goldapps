fe26d1653a1fa0698f21cd01568a61990963d4e7
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-05-27 03:00:35 +0200

    commit button spacing

--- frontend/src/components/SuggestionsTable.tsx
+++ frontend/src/components/SuggestionsTable.tsx
@@ -55,9 +55,9 @@ export const SuggestionsTable = ({
           ))}
         </tbody>
       </table>
-      <div className="flex justify-end">
+      <div className="my-2 flex justify-end">
         <button
-          className="mt-2 rounded bg-blue-500 px-4 py-2 text-white"
+          className="rounded bg-blue-500 px-4 py-2 text-white"
           onClick={onCommit}
         >
           Commit
